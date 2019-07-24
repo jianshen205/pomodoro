4720055dd21b8f38de9dc53d6fe259804dd729b5
jianshen205 <jianshen205@gmail.com>
2019-07-23 23:59:39 -0500

    Initial Commit

---
+++ pomodoro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pomodoro
+//
+//  Created by JianShen on 7/23/19.
+//  Copyright © 2019 JianShen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
