96d372f0a7f85a1b74c515132a69c641a6ea8c1e
jianshen205 <jianshen205@gmail.com>
2019-08-02 19:40:19 -0500

    add demo gifs

---
+++ pomodoro/PDDatePickerCell.swift
@@ -0,0 +1,24 @@
+//
+//  PDDatePickerCell.swift
+//  pomodoro
+//
+//  Created by JianShen on 7/30/19.
+//  Copyright © 2019 JianShen. All rights reserved.
+//
+
+import UIKit
+
+class PDDatePickerCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
